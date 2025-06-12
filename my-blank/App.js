f8ed302d0b9dd6d7064898ed433ac0e7d6525f8d
christian01montalvo <167219601+christian01montalvo@users.noreply.github.com>
2025-06-11 18:25:12 -0600

    correccion primer comit hola mundo

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,37 +1,39 @@
-/*zona de importaciones */
+//importaciones 
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Botton } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
-const Texto=(props)=>{
-  const {contenido}=props
-  return(
-         <Text> {contenido} </Text>
-  )
-}
 
-/* zona2 : main*/
+
+const texto=(props)=>{
+  const{contenido}=props
+   return(
+   <Text>contenido</Text>)}
+    
+
+
+//zona 2 main 
 export default function App() {
   return (
     <View style={styles.container}>
-   
-      <Texto>hola</Texto>
-      <Texto>mundo</Texto>
-      <Texto>react native</Texto>
-      <Botton title='presioname!'></Botton>
-      <StatusBar style="auto" />
+    <StatusBar style="auto" />
 
+      <Text>Hola </Text>
+       <Text> mundo !</Text>
+        <Text> reactnative!</Text>
+      <Button title='Ingresar'></Button>
+     
     </View>
   );
 }
 
 
-/*     zonas 3 de estilos */
-
+//zona 3 estilos 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },
 });
